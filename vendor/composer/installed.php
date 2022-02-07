3b738944789e0cdf894485155b9b44c289acbaca
yuta0402 <y.furukawa@sai.co.jp>
2022-02-07 16:35:19 +0900

    chore: phpmailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'eea88ac8bd783be0cd6b52a89c2d04f67d7540fe',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eea88ac8bd783be0cd6b52a89c2d04f67d7540fe',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.3',
+      'version' => '6.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'baeb7cde6b60b1286912690ab0693c7789a31e71',
+    ),
+  ),
+);
